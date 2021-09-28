eeb5aa29794c40d2eb4ecf516c7f6819775c4aba
HackerY <yitshiji001@163.com>
2021-09-29 01:00:19 +0800

    add vue.config.js and .editorconfig

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  configureWebpack:{
+    resolve:{
+      //起别名配置
+      //默认@ 代表src目录
+      alias:{
+        "assets":"@/assets",
+        "common":"@/common",
+        "components":"@/components",
+        "network":"@/network",
+        "store":"@/store",
+        "views":"@/views"
+      }
+    }
+  }
+}
